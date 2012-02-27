b8bd8e212eeedd1b877f679e4b77d0cd43d02eb4
Viet Nguyen <viet@ooyala.com>
2012-02-26 23:07:45 -0800

    [test] Add jenkins run script.

---
+++ ruby/ci_jobs/hastur-api-libs/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+: ${REPO_ROOT:="$WORKSPACE"}
+source $HOME/.rvm/scripts/rvm
+
+cd $REPO_ROOT/hastur/ruby
+rvm --create use 1.9.3@hastur
+gem install --no-rdoc --no-ri bundler
+bundle install
+rake --trace test:units
