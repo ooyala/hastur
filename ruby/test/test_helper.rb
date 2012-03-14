c923fdfb62f024f7a6cf503af33333eb73c9abd2
Noah Gibbs <noah@ooyala.com>
2012-03-14 08:23:53 -0700

    Fix test coverage for these unit tests

--- ruby/test/test_helper.rb
+++ ruby/test/test_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
 
 if ENV['COVERAGE'] && RUBY_VERSION[/^1.9/]
-  require "simple_cov"
+  require "simplecov"
   SimpleCov.start
 end
