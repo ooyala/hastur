644590a7e1c77e78f685c20dabf9f9f85c801575
Noah Gibbs <noah@ooyala.com>
2012-05-02 21:21:59 -0700

    Modify all_test a bit for Ruby 1.8

--- ruby/test/inclusion/all_test.rb
+++ ruby/test/inclusion/all_test.rb
@@ -7,9 +7,14 @@ class AllInclusionTest < MiniTest::Unit::TestCase
     require "hastur/api"
 
     Hastur.application = "app_name"
-    Hastur.expects(:register_process).with("app_name", {})
+    Hastur.expects(:register_process)
     Hastur.expects(:start_background_thread)
 
     require "hastur/all"
+
+    assert Hastur::RegistrationData.any? do |set|
+      set.has_key?(:loaded_features) &&
+      set.hash_key?(:loaded_gems)
+    end
   end
 end
