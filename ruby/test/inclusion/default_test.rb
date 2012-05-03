113fcb4bfd1d3f229c1117d11fc93c741154971d
Noah Gibbs <noah@ooyala.com>
2012-05-02 21:07:57 -0700

    Test including hastur/default

---
+++ ruby/test/inclusion/default_test.rb
@@ -0,0 +1,15 @@
+require File.join(File.dirname(__FILE__), "..", "test_helper")
+
+require "mocha"
+
+module Hastur
+end
+
+class DefaultInclusionTest < MiniTest::Unit::TestCase
+  def test_inclusion
+    Hastur.expects(:register_process)
+    Hastur.expects(:start_background_thread)
+
+    require "hastur/default"
+  end
+end
