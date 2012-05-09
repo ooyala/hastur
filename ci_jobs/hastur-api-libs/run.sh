f95e0e050315a58c53085726c8be50361219efb5
Noah Gibbs <noah@ooyala.com>
2012-05-09 13:58:16 -0700

    Move Ruby subdir into root

--- ci_jobs/hastur-api-libs/run.sh
+++ ci_jobs/hastur-api-libs/run.sh
@@ -3,7 +3,7 @@
 : ${REPO_ROOT:="$WORKSPACE"}
 source $HOME/.rvm/scripts/rvm
 
-cd $REPO_ROOT/hastur/ruby
+cd $REPO_ROOT/hastur
 rvm --create use 1.9.3@hastur
 gem install --no-rdoc --no-ri bundler
 bundle install
