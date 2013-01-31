e5bc6ac6cff10784b42d9c5a0a5eccc89df53eac
Noah Gibbs <noah@ooyala.com>
2013-01-31 11:05:30 -0800

    First draft conversion to rbenv, expect dependency problems

--- ci_jobs/hastur-api-libs/run.sh
+++ ci_jobs/hastur-api-libs/run.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+set -ex
 
-: ${REPO_ROOT:="$WORKSPACE"}
-source $HOME/.rvm/scripts/rvm
+eval "$(rbenv init -)"
 
-cd $REPO_ROOT/hastur
-rvm --create use 1.9.3@hastur
+# The current working directory is exactly WORKSPACE.
+# This is where each repo the test requires is checked out to.
+: ${OOYALA_REPO_ROOT:="$WORKSPACE"}
+
+# Setup a 1.9.2-p290 test environment
+rbenv shell 1.9.2-p290
+
 gem install --no-rdoc --no-ri bundler
+
+# Move to the project repo
+cd $WORKSPACE/hastur
+
+# install the necessary gems and execute tests
 bundle install
+
+# just in case we installed some executables...
+rbenv rehash
+
 COVERAGE=true bundle exec rake --trace test:units
